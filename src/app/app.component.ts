b62ab2d603dbbbfe661ec3ac541cf14b2fa7175a
Rahul Rao <RAOR378647@exactsoftware.com>
2020-12-13 19:33:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ElementRef, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-observable-from-event';
+  @ViewChild('btn', { static: true }) button: ElementRef;
+  @ViewChild('name', { static: true }) name: ElementRef;
+
+
+  buttonSubscription: any;
+
+  constructor(private elm: ElementRef) {
+  }
+
+  ngOnInit() {
+  }
+
+
+  ngAfterViewInit() {
+    this.buttonClick();
+    this.keyUp();
+    this.scroll();
+  }
+
+
+  buttonClick() {
+    this.buttonSubscription = fromEvent(this.button.nativeElement, 'click')
+      .subscribe(res => console.log("click- ", res));
+  }
+
+
+  ngOnDestroy() {
+    this.buttonSubscription.unsubscribe()
+  }
+
+  scroll() {
+    const source = fromEvent(window, 'scroll');
+    source.subscribe(val => console.log("scroll- ", val));
+  }
+
+  keyUp(){
+    fromEvent(this.name.nativeElement, 'keyup')
+    .subscribe(res => console.log(res));
+  }
+ 
 }
